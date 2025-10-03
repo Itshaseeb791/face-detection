e0b01c67a21f4dd25f85716f9ccba71566f5e0e0
Itshaseeb791 <muh203203@gmail.com>
2025-10-04 01:08:49 +0500

    update to face card

--- src/components/FaceDetectionCamera.jsx
+++ src/components/FaceDetectionCamera.jsx
@@ -1,108 +1,3 @@
-// import React, { useRef, useEffect } from "react";
-// import * as faceapi from "face-api.js";
-
-// const FaceBox = () => {
-//   const videoRef = useRef(null);
-//   const canvasRef = useRef(null);
-
-//   // Start webcam
-//   const startVideo = async () => {
-//     try {
-//       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-//       videoRef.current.srcObject = stream;
-//     } catch (err) {
-//       console.error("Error accessing webcam:", err);
-//     }
-//   };
-
-//   const detectFaces = async () => {
-//   if (videoRef.current) {
-//     const detections = await faceapi.detectAllFaces(
-//       videoRef.current,
-//       new faceapi.TinyFaceDetectorOptions({ inputSize: 320, scoreThreshold: 0.5 })
-//     );
-
-//     const resized = faceapi.resizeResults(detections, {
-//       width: videoRef.current.width,
-//       height: videoRef.current.height,
-//     });
-
-//     const context = canvasRef.current.getContext("2d");
-//     context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-
-//     faceapi.draw.drawDetections(canvasRef.current, resized);
-//   }
-
-//   requestAnimationFrame(detectFaces);
-// };
-
-
-//   useEffect(() => {
-//     const loadModel = async () => {
-//       await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
-
-//       startVideo();
-//     };
-//     loadModel();
-
-//     const handlePlay = () => {
-//       const canvas = canvasRef.current;
-//       const displaySize = {
-//         width: videoRef.current.width,
-//         height: videoRef.current.height,
-//       };
-//       faceapi.matchDimensions(canvas, displaySize);
-
-//       setInterval(async () => {
-//         const detections = await faceapi.detectAllFaces(
-//           videoRef.current,
-//           new faceapi.TinyFaceDetectorOptions()
-//         );
-
-//         const resized = faceapi.resizeResults(detections, displaySize);
-//         const context = canvas.getContext("2d");
-//         context.clearRect(0, 0, canvas.width, canvas.height);
-
-//         faceapi.draw.drawDetections(canvas, resized);
-//       }, 100);
-//     };
-
-//     videoRef.current?.addEventListener("playing", handlePlay);
-//     detectFaces()
-
-//     return () => {
-//       videoRef.current?.removeEventListener("playing", handlePlay);
-//     };
-//   }, []);
-
-//   return (
-//     <> 
-//           <h1 className="text-3xl font-bold text-blue-600  drop-shadow">
-//         React Face Detection
-//       </h1>
-//     <div className="">
-//       <video
-//         ref={videoRef}
-//         autoPlay
-//         muted
-//         width="640"
-//         height="480"
-//         style={{ position: "absolute" }}
-//       />
-//       <canvas
-//         ref={canvasRef}
-//         width="640"
-//         height="480"
-//         style={{ position: "absolute" }}
-//       />
-//     </div>
-//     </>
-//   );
-// };
-
-// export default FaceBox;
-
-
 import React, { useRef, useEffect } from "react";
 import * as faceapi from "face-api.js";
 
@@ -121,29 +16,31 @@ const FaceBox = () => {
   };
 
   const detectFaces = async () => {
-    if (videoRef.current) {
-      const detections = await faceapi.detectAllFaces(
-        videoRef.current,
-        new faceapi.TinyFaceDetectorOptions({ inputSize: 320, scoreThreshold: 0.5 })
-      );
+  if (videoRef.current) {
+    const detections = await faceapi.detectAllFaces(
+      videoRef.current,
+      new faceapi.TinyFaceDetectorOptions({ inputSize: 320, scoreThreshold: 0.5 })
+    );
 
-      const resized = faceapi.resizeResults(detections, {
-        width: videoRef.current.width,
-        height: videoRef.current.height,
-      });
+    const resized = faceapi.resizeResults(detections, {
+      width: videoRef.current.width,
+      height: videoRef.current.height,
+    });
 
-      const context = canvasRef.current.getContext("2d");
-      context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    const context = canvasRef.current.getContext("2d");
+    context.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
 
-      faceapi.draw.drawDetections(canvasRef.current, resized);
-    }
+    faceapi.draw.drawDetections(canvasRef.current, resized);
+  }
 
-    requestAnimationFrame(detectFaces);
-  };
+  requestAnimationFrame(detectFaces);
+};
+
 
   useEffect(() => {
     const loadModel = async () => {
       await faceapi.nets.tinyFaceDetector.loadFromUri("/models");
+
       startVideo();
     };
     loadModel();
@@ -171,7 +68,7 @@ const FaceBox = () => {
     };
 
     videoRef.current?.addEventListener("playing", handlePlay);
-    detectFaces();
+    detectFaces()
 
     return () => {
       videoRef.current?.removeEventListener("playing", handlePlay);
@@ -179,31 +76,30 @@ const FaceBox = () => {
   }, []);
 
   return (
-    <div className="">
-      <h1 className="mb-6 text-3xl font-bold text-blue-600 drop-shadow">
+    <> 
+          <h1 className="text-3xl font-bold text-blue-600  drop-shadow">
         React Face Detection
       </h1>
-
-     <div className="">
-       <video
+    <div className="">
+      <video
         ref={videoRef}
         autoPlay
         muted
-        width="600"
-        height="450"
+        width="640"
+        height="480"
         style={{ position: "absolute" }}
       />
       <canvas
         ref={canvasRef}
-        width="600"
-        height="450"
+        width="640"
+        height="480"
         style={{ position: "absolute" }}
       />
     </div>
-
-
-    </div>
+    </>
   );
 };
 
 export default FaceBox;
+
+
